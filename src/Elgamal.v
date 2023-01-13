d539e008d5a8011f17be84ea2cc0ebdbc3303a3d
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2023-01-13 13:28:49 +1100

    Minor cleanup

--- src/Elgamal.v
+++ src/Elgamal.v
@@ -35,7 +35,7 @@ Section Elgamal.
     (g h : G) (* group generator and public key *)
     (Hk : h = g^x). (* assumption that public key and private key are related *)
 
-  Section EncDec.  
+  Section EncDecDef.
 
     Definition enc (m : F) (r : F) : G * G := 
       (g^r, gop (g^m) (h^r)).
@@ -45,6 +45,21 @@ Section Elgamal.
       |(c₁, c₂) => gop c₂ (ginv (c₁^x))
       end.
 
+    (* Re Encryption of cipher text *)
+    Definition re_enc (c : G * G) (r : F) : G * G := 
+      match c with
+      |(c₁, c₂) => (gop c₁ (g^r), gop c₂ (h^r))
+      end.
+
+     (* mulitply two cipher text *)
+    Definition mul_cipher (c₁ c₂ : G * G) : G * G :=
+      match c₁, c₂ with
+      |(c₁, d₁), (c₂, d₂) => (gop c₁ c₂, gop d₁ d₂)
+      end.
+  
+  End EncDecDef.
+
+  Section EncDecProof.
     
     (* encryption and decryption lemma.
       Note that we are getting g^m back, not m, 
@@ -75,11 +90,7 @@ Section Elgamal.
     Qed.
     
     
-    (* Re Encryption of cipher text *)
-    Definition re_enc (c : G * G) (r : F) : G * G := 
-      match c with
-      |(c₁, c₂) => (gop c₁ (g^r), gop c₂ (h^r))
-      end.
+    
 
     
     (* re_encryption decrypts to the same value *)
@@ -125,11 +136,7 @@ Section Elgamal.
     Qed.
     
     
-    (* mulitply two cipher text *)
-    Definition mul_cipher (c₁ c₂ : G * G) : G * G :=
-      match c₁, c₂ with
-      |(c₁, d₁), (c₂, d₂) => (gop c₁ c₂, gop d₁ d₂)
-      end.
+   
 
     (* 
     Not working! 
@@ -178,179 +185,161 @@ Section Elgamal.
       typeclasses eauto.
     Qed.
 
-  End EncDec.
+  End EncDecProof.
 
-  (* Not needed for now *)
+
   Section Ballot.
 
 
-   
+    Section BallotDef.
+
+
+      Definition encrypted_ballot {n : nat} 
+        (ms rs : Vector.t F n) : Vector.t (G * G) n :=
+        zip_with (fun m r => enc m r) ms rs.
+
+      Definition decrypted_ballot {n : nat} 
+        (cs : Vector.t (G * G) n) : Vector.t G n :=
+        map (fun c => dec c) cs.
+
+      Definition re_encryption_ballot {n : nat} 
+        (cs : Vector.t (G * G) n) (rs : Vector.t F n) : 
+        Vector.t (G * G) n :=
+        zip_with (fun c r => re_enc c r) cs rs.
+
+      Definition mul_encrypted_ballots {n : nat} 
+        (us vs : Vector.t (G * G) n) : Vector.t (G * G) n := 
+        zip_with (fun c₁ c₂ => mul_cipher c₁ c₂) us vs.
+
+      Definition raise_message_gen {n : nat} 
+        (ms : Vector.t F n) : Vector.t G n := 
+        map (fun m => g^m) ms.
+    
+    End BallotDef.
 
     
+    Section BallotProof.
+      (* When we decrypt an encrypted ballot, we get group element, g^m, and 
+        one way to recover this m is to write a formally verified discrete logarithm 
+        algorithm, or write an OCaml code *)
+      Lemma ballot_encryption_decryption_raise_message: 
+        forall (n : nat) (ms rs : Vector.t F n) (cs :  Vector.t (G * G) n), 
+        cs = encrypted_ballot ms rs -> 
+        raise_message_gen ms = decrypted_ballot cs.
+      Proof.
+        induction n.
+        + intros ? ? cs ->. 
+          refine (match ms, rs with 
+            | nil _, nil _ => _ 
+            end).
+          reflexivity.
+        + intros ? ? cs ->.
+          pose proof (vector_inv_S ms) as (hms & tms & Hms).
+          pose proof (vector_inv_S rs) as (hrs & trs & Hrs).
+          rewrite Hms, Hrs; simpl. 
+          f_equal.
+          rewrite Hk. 
+          repeat rewrite smul_pow_up, 
+          smul_pow_mul, <-associative, right_inverse, 
+          right_identity; reflexivity.
+          apply IHn with  (rs := tl rs).
+          unfold encrypted_ballot.
+          rewrite Hrs; simpl.
+          reflexivity.
+      Qed.
 
 
-
-    Definition encrypted_ballot {n : nat} 
-      (ms rs : Vector.t F n) : Vector.t (G * G) n :=
-      zip_with (fun m r => enc m r) ms rs.
-
-    Definition decrypted_ballot {n : nat} 
-      (cs : Vector.t (G * G) n) : Vector.t G n :=
-      map (fun c => dec c) cs.
-
-    Definition re_encryption_ballot {n : nat} 
-      (cs : Vector.t (G * G) n) (rs : Vector.t F n) : 
-      Vector.t (G * G) n :=
-      zip_with (fun c r => re_enc c r) cs rs.
-
-    Definition mul_encrypted_ballots {n : nat} 
-      (us vs : Vector.t (G * G) n) : Vector.t (G * G) n := 
-      zip_with (fun c₁ c₂ => mul_cipher c₁ c₂) us vs.
-
-    Definition raise_message_gen {n : nat} 
-      (ms : Vector.t F n) : Vector.t G n := 
-      map (fun m => g^m) ms.
-
-    Lemma vector_inv_0 {R : Type} (v : Vector.t R 0) :
-      v = @Vector.nil R.
-    Proof.
-      refine (match v with
-              | @Vector.nil _ => _
-              end).
-      reflexivity.
-    Defined.
-  
-    Lemma vector_inv_S {R : Type} (n : nat) (v : Vector.t R (S n)) :
-      {x & {v' | v = @Vector.cons _ x _ v'}}.
-    Proof.
-      refine (match v with
-              | @Vector.cons _ x _ v' =>  _
-              end);
-      eauto.
-    Defined.
-    
-    
-    (* When we decrypt an encrypted ballot, we get group element, g^m, and 
-      one way to recover this m is to write a formally verified discrete logarithm 
-      algorithm, or write an OCaml code *)
-    Lemma ballot_encryption_decryption_raise_message: 
-      forall (n : nat) (ms rs : Vector.t F n) (cs :  Vector.t (G * G) n), 
-      cs = encrypted_ballot ms rs -> 
-      raise_message_gen ms = decrypted_ballot cs.
-    Proof.
-      induction n.
-      + intros ? ? cs ->. 
-        refine (match ms, rs with 
-          | nil _, nil _ => _ 
-          end).
-        reflexivity.
-      + intros ? ? cs ->.
-        pose proof (vector_inv_S _ ms) as (hms & tms & Hms).
-        pose proof (vector_inv_S _ rs) as (hrs & trs & Hrs).
-        rewrite Hms, Hrs; simpl. 
-        f_equal.
-        rewrite Hk. 
-        repeat rewrite smul_pow_up, 
-        smul_pow_mul, <-associative, right_inverse, 
-        right_identity; reflexivity.
-        apply IHn with  (rs := tl rs).
-        unfold encrypted_ballot.
-        rewrite Hrs; simpl.
-        reflexivity.
-    Qed.
-
-
-    Lemma ballot_re_encryption_decryption_raise_message : 
-      forall (n : nat) (ms rs₁ rs₂: Vector.t F n) cs₁ cs₂, 
-      cs₁ = encrypted_ballot ms rs₁ -> 
-      cs₂ = re_encryption_ballot cs₁ rs₂ -> 
-      raise_message_gen ms = decrypted_ballot cs₂.
-    Proof.
-      induction n.
-      + intros ? ? ? ? ? Hcs₁ ->. 
-        rewrite Hcs₁; clear Hcs₁; 
-        clear cs₁.
-        refine (match ms, rs₁, rs₂ with 
-          | nil _, nil _, nil _ => _ 
-          end).
-        reflexivity.
-      + intros ? ? ? ? ? Hcs₁ ->. 
-        rewrite Hcs₁; clear Hcs₁; clear cs₁.
-        pose proof (vector_inv_S _ ms) as (hms & tms & Hms).
-        pose proof (vector_inv_S _ rs₁) as (hrs₁ & trs₁ & Hrs₁).
-        pose proof (vector_inv_S _ rs₂) as (hrs₂ & trs₂ & Hrs₂).
-        rewrite Hms, Hrs₁, Hrs₂; simpl.
-        f_equal.
-        rewrite Hk. 
-        repeat rewrite smul_pow_up,
-        smul_pow_mul.
-        assert (Ht : 
-          (gop (gop (g ^ hms) (g ^ (x * hrs₁))) 
-              (g ^ (x * hrs₂))) = 
-          (gop (g ^ hms) 
-               (gop (g ^ (x * hrs₁)) 
-                    (g ^ (x * hrs₂))))).
-        rewrite <- (@monoid_is_associative G (@eq G) gop gid). 
-        reflexivity. 
-        typeclasses eauto.
-        assert (Htv : hrs₂ * x = x * hrs₂). 
-        rewrite commutative;
-        reflexivity. 
-        rewrite <-Htv in Ht.  
-        rewrite Ht. 
-        clear Ht.
-        rewrite <- smul_distributive_fadd.
-        rewrite <- (@monoid_is_associative G (@eq G) gop gid).
-        assert (Htt: gop (g ^ hrs₁) (g ^ hrs₂) ^ x = 
-          gop (g ^ (hrs₁ * x)) (g ^ (hrs₂ * x))).
-        rewrite <- smul_distributive_fadd.
-        rewrite <- (@vector_space_smul_associative_fmul 
+      Lemma ballot_re_encryption_decryption_raise_message : 
+        forall (n : nat) (ms rs₁ rs₂: Vector.t F n) cs₁ cs₂, 
+        cs₁ = encrypted_ballot ms rs₁ -> 
+        cs₂ = re_encryption_ballot cs₁ rs₂ -> 
+        raise_message_gen ms = decrypted_ballot cs₂.
+      Proof.
+        induction n.
+        + intros ? ? ? ? ? Hcs₁ ->. 
+          rewrite Hcs₁; clear Hcs₁; 
+          clear cs₁.
+          refine (match ms, rs₁, rs₂ with 
+            | nil _, nil _, nil _ => _ 
+            end).
+          reflexivity.
+        + intros ? ? ? ? ? Hcs₁ ->. 
+          rewrite Hcs₁; clear Hcs₁; clear cs₁.
+          pose proof (vector_inv_S ms) as (hms & tms & Hms).
+          pose proof (vector_inv_S rs₁) as (hrs₁ & trs₁ & Hrs₁).
+          pose proof (vector_inv_S rs₂) as (hrs₂ & trs₂ & Hrs₂).
+          rewrite Hms, Hrs₁, Hrs₂; simpl.
+          f_equal.
+          rewrite Hk. 
+          repeat rewrite smul_pow_up,
+          smul_pow_mul.
+          assert (Ht : 
+            (gop (gop (g ^ hms) (g ^ (x * hrs₁))) 
+                (g ^ (x * hrs₂))) = 
+            (gop (g ^ hms) 
+                (gop (g ^ (x * hrs₁)) 
+                      (g ^ (x * hrs₂))))).
+          rewrite <- (@monoid_is_associative G (@eq G) gop gid). 
+          reflexivity. 
+          typeclasses eauto.
+          assert (Htv : hrs₂ * x = x * hrs₂). 
+          rewrite commutative;
+          reflexivity. 
+          rewrite <-Htv in Ht.  
+          rewrite Ht. 
+          clear Ht.
+          rewrite <- smul_distributive_fadd.
+          rewrite <- (@monoid_is_associative G (@eq G) gop gid).
+          assert (Htt: gop (g ^ hrs₁) (g ^ hrs₂) ^ x = 
+            gop (g ^ (hrs₁ * x)) (g ^ (hrs₂ * x))).
+          rewrite <- smul_distributive_fadd.
+          rewrite <- (@vector_space_smul_associative_fmul 
+              F (@eq F) zero one add mul sub div 
+              opp inv G (@eq G) gid ginv gop gpow).
+          rewrite (@commutative_ring_is_commutative F (@eq F) zero one opp add sub mul).
+          rewrite (@ring_is_left_distributive F (@eq F) zero one opp add sub mul).
+          rewrite (@vector_space_smul_distributive_fadd 
             F (@eq F) zero one add mul sub div 
             opp inv G (@eq G) gid ginv gop gpow).
-        rewrite (@commutative_ring_is_commutative F (@eq F) zero one opp add sub mul).
-        rewrite (@ring_is_left_distributive F (@eq F) zero one opp add sub mul).
-        rewrite (@vector_space_smul_distributive_fadd 
-          F (@eq F) zero one add mul sub div 
-          opp inv G (@eq G) gid ginv gop gpow).
-        assert (Hwt₁: x * hrs₁ = hrs₁ * x). 
-        rewrite commutative; reflexivity.
-        rewrite Hwt₁.
-        assert (Hwt₂: x * hrs₂ = hrs₂ * x). 
-        rewrite commutative; reflexivity.
-        rewrite Hwt₂. 
-        reflexivity.
-        typeclasses eauto.
-        typeclasses eauto.
-        typeclasses eauto.
-        typeclasses eauto.
-        rewrite Htt.
-        rewrite <- (@vector_space_smul_distributive_fadd 
-          F (@eq F) zero one add mul sub div 
-          opp inv G (@eq G) gid ginv gop gpow).
-        assert (Hwt₁: x * hrs₁ = hrs₁ * x). 
-        rewrite commutative; reflexivity.
-        rewrite <-Hwt₁.
-        assert (Hwt₂: x * hrs₂ = hrs₂ * x). 
-        rewrite commutative; reflexivity.
-        rewrite <-Hwt₂. 
-        rewrite right_inverse, right_identity;
-        reflexivity. 
-        typeclasses eauto. 
-        typeclasses eauto.
-        exact (IHn tms trs₁ trs₂ 
-          (encrypted_ballot tms trs₁) 
-          (re_encryption_ballot 
-            (encrypted_ballot tms trs₁) trs₂)
-          eq_refl eq_refl).
-    Qed.
-    
-    
+          assert (Hwt₁: x * hrs₁ = hrs₁ * x). 
+          rewrite commutative; reflexivity.
+          rewrite Hwt₁.
+          assert (Hwt₂: x * hrs₂ = hrs₂ * x). 
+          rewrite commutative; reflexivity.
+          rewrite Hwt₂. 
+          reflexivity.
+          typeclasses eauto.
+          typeclasses eauto.
+          typeclasses eauto.
+          typeclasses eauto.
+          rewrite Htt.
+          rewrite <- (@vector_space_smul_distributive_fadd 
+            F (@eq F) zero one add mul sub div 
+            opp inv G (@eq G) gid ginv gop gpow).
+          assert (Hwt₁: x * hrs₁ = hrs₁ * x). 
+          rewrite commutative; reflexivity.
+          rewrite <-Hwt₁.
+          assert (Hwt₂: x * hrs₂ = hrs₂ * x). 
+          rewrite commutative; reflexivity.
+          rewrite <-Hwt₂. 
+          rewrite right_inverse, right_identity;
+          reflexivity. 
+          typeclasses eauto. 
+          typeclasses eauto.
+          exact (IHn tms trs₁ trs₂ 
+            (encrypted_ballot tms trs₁) 
+            (re_encryption_ballot 
+              (encrypted_ballot tms trs₁) trs₂)
+            eq_refl eq_refl).
+      Qed.
+
+    End BallotProof.
+  
   End Ballot.
-        
 
 End Elgamal.
 
-(* What about security proof of Elgamal Method? *)
+(* Implement security proof of Elgamal Method? *)
 
 
 
